91515148fdda05bcbd77faea6042f4421530cf8c
TaiHV <72026756+VanTai0211@users.noreply.github.com>
2021-09-09 09:30:45 +0700

    First_Create_New_Project

---
+++ B1_CNNet_COMP1064_in_Class/B1_CNNet_COMP1064_in_Class/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace B1_CNNet_COMP1064_in_Class.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
