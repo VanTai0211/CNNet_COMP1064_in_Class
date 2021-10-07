9f7a65b8372d9218c15d0431da62ec07d8b48ec5
VanTai0211 <vantai02112001@gmail.com>
2021-10-07 09:10:37 +0700

    employee

---
+++ Employee_crud/Employee_crud/Controllers/EmployeeController.cs
@@ -0,0 +1,68 @@
+﻿using Employee_crud.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_crud.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public static List<Employee> employees = new List<Employee>();
+        public IActionResult Index()
+        {
+
+            return View(employees);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            var _employee = employees.FirstOrDefault(p => p.IdEmployee == employee.IdEmployee);
+            if (_employee == null)
+            {
+                employees.Add(employee);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit (int id)
+        {
+            var employee = employees.FirstOrDefault(p => p.IdEmployee == id);
+            if(employee != null)
+            {
+                return View(employee);
+            }
+
+            return RedirectToAction("Index", "Employee");
+        }
+        [HttpPost]
+        public IActionResult Edit (Employee employee)
+        {
+            var _employee = employees.FirstOrDefault(p => p.IdEmployee == employee.IdEmployee);
+            if (_employee != null)
+            {
+                _employee.NameEmployee = employee.NameEmployee;
+                _employee.AgeEmployee = employee.AgeEmployee;
+                _employee.DailySalary = employee.DailySalary;
+                _employee.NumberDay = employee.NumberDay;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var _employee = employees.FirstOrDefault(p => p.IdEmployee == id);
+            if (_employee != null)
+            {
+                employees.Remove(_employee);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Employee_crud/Employee_crud/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_crud.Models
+{
+    public class Employee
+    {
+        [Display(Name = "ID Nhân Viên")]
+        [Required]
+        public int IdEmployee { get; set; }
+        [Required]
+        [Display(Name = "Tên Nhân Viên")]
+        [MinLength (1,ErrorMessage ="Tối thiểu 1 kí tự")]
+        public string NameEmployee { get; set; }
+        [Range(16,50,ErrorMessage ="Tôis thiểu 16 tuổi")]
+        [Display(Name = "Tuổi Nhân Viên")]
+
+        public int AgeEmployee { get; set; }
+        [Display(Name = "Lương theo ngày của Nhân Viên")]
+
+        public double DailySalary { get; set; }
+        [Display(Name = "Số ngày làm việc Nhân Viên")]
+        public int NumberDay { get; set; }
+        [Display(Name = "Lương Nhân Viên")]
+        public double EmployeeWages => DailySalary * NumberDay;
+    }
+}
